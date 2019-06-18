51f5955e17da3604e080c34731357d672b39b8b6
nejigram <nejigram@gmail.com>
2019-06-19 03:46:28 +0900

    first commit

---
+++ index.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width,initial-scale=1.0,minimum-scale=1.0">
+
+<link rel="shortcut icon" type="image/x-icon" href="//www.nejigram.com/assets/img/itemicon.ico" />
+<title>bmi</title>
+<link rel='stylesheet' href='css/style.css?<?=time();?>' type='text/css' />
+<script src='js/bmi.js?<?=time();?>' async></script>
+</head>
+<body>
+<header>
+<h1>bmi 計算機</h1>
+</header>
+<div class="box font_sawarabi_gothic">
+<form>
+<p class="tc heightline">身長 : <input type="number" id="height"> cm</p>
+<p class="tc">体重 : <input type="number" id="weight"> kg</p>
+</form>
+<p class="tc bmiline">bmi : <span id="bmi" class="">----</span></p>
+
+    <div class="result ja" id="result1">
+        <p>18.5未満　低体重</p>
+        <p>18.5～25未満	普通体重</p>
+        <p>25～30未満	肥満（１度）</p>
+        <p>30～35未満	肥満（２度）</p>
+        <p>35～40未満	肥満（３度）</p>
+        <p>40以上	肥満（４度）</p>
+    </div>
+    <div class="result who" id="result2">
+        <p>16未満　痩せすぎ</p>
+        <p>16～17未満　痩せ</p>
+        <p>17～18.5未満　痩せぎみ</p>
+        <p>18.5～25未満　普通体重</p>
+        <p>25～30未満　過体重</p>
+        <p>30～35未満　肥満（１度）</p>
+        <p>35～40未満　肥満（２度）</p>
+        <p>40以上　肥満（３度）</p>
+    </div>
+</div>
+
+</body>
+</html>
---
+++ js/bmi.js
@@ -0,0 +1,56 @@
+const height = document.querySelector("#height");
+const weight = document.querySelector("#weight");
+const bmi = document.querySelector("#bmi");
+const result1 = document.querySelectorAll("#result1 p");
+const result2 = document.querySelectorAll("#result2 p");
+const bmi_calc = function(h = 0,w = 0){
+    if(h <= 0 || w <= 0){
+        return false;
+    }
+
+    hm = h / 100;
+    const bminum = (Math.ceil((w / (hm * hm)) * 100)) / 100
+    bmi.innerHTML = bminum;
+console.log(result1);
+    for(let x = 0;x < result1.length;x++){
+        result1[x].classList.remove("bbox_bk_5");
+
+    }
+    for(let x = 0;x < result2.length;x++){
+        result2[x].classList.remove("bbox_bk_5");
+    }
+    if(bminum < 16){
+        result1[0].classList.add("bbox_bk_5");
+        result2[0].classList.add("bbox_bk_5");
+    }else if(bminum < 17){
+        result1[0].classList.add("bbox_bk_5");
+        result2[1].classList.add("bbox_bk_5");
+    }else if(bminum < 18.5){
+        result1[0].classList.add("bbox_bk_5");
+        result2[2].classList.add("bbox_bk_5");
+    }else if(bminum < 25){
+        result1[1].classList.add("bbox_bk_5");
+        result2[3].classList.add("bbox_bk_5");
+    }else if(bminum < 30){
+        result1[2].classList.add("bbox_bk_5");
+        result2[4].classList.add("bbox_bk_5");
+    }else if(bminum < 35){
+        result1[3].classList.add("bbox_bk_5");
+        result2[5].classList.add("bbox_bk_5");
+    }else if(bminum < 40){
+        result1[4].classList.add("bbox_bk_5");
+        result2[6].classList.add("bbox_bk_5");
+    }else if(40 <= bminum){
+        result1[5].classList.add("bbox_bk_5");
+        result2[7].classList.add("bbox_bk_5");
+    }
+
+}
+
+height.addEventListener("input",function(){
+    bmi_calc(height.value,weight.value);
+});
+
+weight.addEventListener("input",function(){
+    bmi_calc(height.value,weight.value);
+})
